4a9568f06ddf80acae02f6ca0e8c8eb9dc379ad2
IgorCRH <125159948+IgorCRH@users.noreply.github.com>
2024-01-01 17:18:13 -0300

    Add files via upload

---
+++ Net/IO/PacketBuilder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Chat.Net.IO
+{
+    class PacketBuilder
+    {
+        MemoryStream ms;
+
+        public PacketBuilder()
+        {
+            ms = new MemoryStream();
+        }
+
+        public void EscritaOpCode(byte opcode) {
+            ms.WriteByte(opcode);
+        }
+
+        public void EscritaString(string mensagem)
+        {
+            // Converta a string para bytes usando UTF-8
+            byte[] mensagemBytes = Encoding.UTF8.GetBytes(mensagem);
+
+            // Escreva o tamanho da mensagem como um inteiro de 4 bytes (usando little-endian)
+            ms.Write(BitConverter.GetBytes(mensagemBytes.Length), 0, 4);
+
+            // Escreva os bytes da mensagem
+            ms.Write(mensagemBytes, 0, mensagemBytes.Length);
+        }
+
+
+        public byte[] GetPackedBytes()
+        {
+            return ms.ToArray();
+        }
+    }
+}
---
+++ Net/IO/PacketReader.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Chat.Net.IO
+{
+    class PacketReader : BinaryReader
+    {
+        private NetworkStream ns;
+
+        public PacketReader(NetworkStream ns) : base(ns)
+        {
+            this.ns = ns;
+        }
+
+        public string LeituraMensagem()
+        {
+            int tamanhoMensagem = ReadInt32();  // Substitui LeituraInt32 pela função ReadInt32
+
+            // Certifique-se de que o tamanho da mensagem seja válido
+            if (tamanhoMensagem < 0)
+            {
+                throw new InvalidOperationException("Tamanho de mensagem inválido.");
+            }
+
+            byte[] buffer = new byte[tamanhoMensagem];
+            int totalBytesRead = 0;
+
+            while (totalBytesRead < tamanhoMensagem)
+            {
+                int bytesRead = ns.Read(buffer, totalBytesRead, tamanhoMensagem - totalBytesRead);  // Substitui stream por ns
+
+                if (bytesRead == 0)
+                {
+                    // Se bytesRead for 0, significa que não há mais dados para ler
+                    throw new InvalidOperationException("Não há mais dados para ler.");
+                }
+
+                totalBytesRead += bytesRead;
+            }
+
+            return Encoding.UTF8.GetString(buffer, 0, tamanhoMensagem);
+        }
+    }
+}
