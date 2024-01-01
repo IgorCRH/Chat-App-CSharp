9d779bbac0b605931db0c711515e4897ef787652
IgorCRH <125159948+IgorCRH@users.noreply.github.com>
2024-01-01 17:23:49 -0300

    Add files via upload

---
+++ ChatServidor/Cliente.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using Chat.Net.IO;
+
+namespace ChatServidor
+{
+    class Cliente
+    {
+        public string Usuario { get; set; }
+        public Guid ID { get; set; }
+        public TcpClient SocketCliente { get; set; }
+
+        PacketReader packetLeitura;
+
+        public Cliente(TcpClient cliente)
+        {
+            this.SocketCliente = cliente;
+            this.ID = Guid.NewGuid();
+            packetLeitura = new PacketReader(SocketCliente.GetStream());
+
+            var opcode = packetLeitura.ReadByte();
+            Usuario = packetLeitura.LeituraMensagem();
+
+            Console.WriteLine($"[{DateTime.Now}]: Cliente conectou como usuario {Usuario}");
+
+            Task.Run(() => Processar());
+        }
+
+        void Processar()
+        {
+            try
+            {
+                var opcode = packetLeitura.ReadByte();
+                switch (opcode)
+                {
+                    case 5:
+                        var mensagem = packetLeitura.LeituraMensagem();
+                        Console.WriteLine($"[{DateTime.Now}]: Mensagem recebida {Usuario} : {mensagem}");
+                        Program.TransmitirMensagem($"[{DateTime.Now}] - {Usuario} : {mensagem}");
+                        break;
+                    default:
+                        Console.WriteLine("Nada a ocorrer.");
+                        break;
+                }
+            }
+            catch (Exception)
+            {
+                Program.TransmitirMensagemDesconexao(ID.ToString());
+                SocketCliente.Close();
+                throw;
+            }
+        }
+
+    }
+}
---
+++ ChatServidor/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using Chat.Net.IO;
+
+namespace ChatServidor
+{
+    class Program
+    {
+        static List<Cliente> usuarios;
+        static TcpListener escutador;
+
+        static void Main(string[] args)
+        {
+            usuarios = new List<Cliente>();
+            escutador = new TcpListener(IPAddress.Parse("127.0.0.1"), 8080);
+            escutador.Start();
+            Console.WriteLine("Cliente lançado.");
+
+            // Inicie o loop para aceitar conexões continuamente
+            while (true)
+            {
+                // Aguarde e aceite conexões de clientes de forma assíncrona
+                TcpClient tcpClient = escutador.AcceptTcpClient();
+
+                // Processar o cliente conectado
+                var cliente = new Cliente(tcpClient);
+                usuarios.Add(cliente);
+
+                TransmitirConexao();
+            }
+        }
+
+        static void TransmitirConexao()
+        {
+            foreach (var usuario in usuarios)
+            {
+
+                    var packetTransmissao = new PacketBuilder();
+                    packetTransmissao.EscritaOpCode(1);
+                    packetTransmissao.EscritaString(usuario.Usuario);
+                    packetTransmissao.EscritaString(usuario.ID.ToString());
+                    usuario.SocketCliente.Client.Send(packetTransmissao.GetPackedBytes());
+                    Console.WriteLine("Conexao sendo iniciada");
+            }
+        }
+
+        public static void TransmitirMensagem(string mensagem)
+        {
+            foreach (var usador in usuarios)
+            {
+                var packetMensagem = new PacketBuilder();
+                packetMensagem.EscritaOpCode(5);
+                packetMensagem.EscritaString(mensagem);
+                usador.SocketCliente.Client.Send(packetMensagem.GetPackedBytes());
+            }
+        }
+
+        public static void TransmitirMensagemDesconexao(string ID)
+        {
+            var usuarioDesconectado = usuarios.Find(x => x.ID.ToString() == ID);
+            usuarios.Remove(usuarioDesconectado);
+
+            foreach (var usador in usuarios)
+            {
+                var packetTransmissao = new PacketBuilder();
+                packetTransmissao.EscritaOpCode(10);
+                packetTransmissao.EscritaString(ID);
+                usador.SocketCliente.Client.Send(packetTransmissao.GetPackedBytes());
+            }
+
+            TransmitirMensagem($"[{usuarioDesconectado.Usuario}] Desconectado!");
+        }
+    }
+}
